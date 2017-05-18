f672f9885c3a87b0c9d00ed3cb254db8cc251139
Jurgen <jazuk.jurij@gmail.com>
2017-05-18 10:05:53 +0300

    add new  1 cout

--- test_1/test_1_git/main.cpp
+++ test_1/test_1_git/main.cpp
@@ -7,5 +7,6 @@ int main(int argc, char *argv[])
     cout << "Hello World!" << endl;
     cout << "Test project 1" << endl;
     cout << "Test project 2" << endl;
+     cout << "Test project 3" << endl;
     return 0;
 }
