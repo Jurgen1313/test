581e43ab1a775a12a2b39ef6f13564987b2abedf
Jurgen <jazuk.jurij@gmail.com>
2017-05-18 10:34:22 +0300

    send

--- test_1/test_1_git/main.cpp
+++ test_1/test_1_git/main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
     cout << "Hello World!" << endl;
     cout << "Test project 1" << endl;
     cout << "Test project 2" << endl;
-     cout << "Test project 3" << endl;
+    cout << "Test project 3" << endl;
+    cout << "Test project 4" << endl;
     return 0;
 }
