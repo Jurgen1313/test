d379d93b60e71677f3aaf214beca985bed245af1
Jurgen <jazuk.jurij@gmail.com>
2017-05-19 12:45:19 +0300

    second program v1.0

---
+++ c++/HW1/2/2/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int input_number;
+
+int main()
+{
+    cout << "\t Welcome\n";
+    cout << "Type 3 digits number : ";
+    cin >> input_number;
+    cout << endl;
+    cout << input_number;
+    return 0;
+}
