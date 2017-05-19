29035c26b7a5f880fffb1b8debfc21c78a3656df
Jurgen <jazuk.jurij@gmail.com>
2017-05-19 10:51:11 +0300

    working programm without checking intager

--- c++/HW1/1/HW1_1/main.cpp
+++ c++/HW1/1/HW1_1/main.cpp
@@ -10,7 +10,31 @@
 using namespace std;
 
 int main()
-{
-    cout << "Hello World!" << endl;
+{   
+    int input_number;
+    int max_number = 0;
+    int numbers_count = 0;
+    int number_index = 0;
+
+    cout << "\t Welcome\n";
+    cout << "How many intager numbers do you want to input?\n";
+    cout << "n = ";
+    cin >> numbers_count;
+    cout << endl;
+
+    for ( int i = 0; i < numbers_count; i++)
+    {
+        cout << " Give me intager number [" << i + 1 << "] = ";
+        cin >> input_number;
+
+        if ( input_number > max_number )
+        {
+            max_number = input_number;
+            number_index = i +1;
+        }
+    }
+
+    cout << "\nThe maximum value is number [" << number_index << "] = " << max_number << "\n\n";
+
     return 0;
 }
