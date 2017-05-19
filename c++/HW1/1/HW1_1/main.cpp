67c7d606a961f7fc620310382b29261ba387d5a8
Jurgen <jazuk.jurij@gmail.com>
2017-05-19 09:58:39 +0300

    New project

---
+++ c++/HW1/1/HW1_1/main.cpp
@@ -0,0 +1,16 @@
+/*
+ *
+ * Program reads five intagers and
+ * prints the highest number
+ *
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
