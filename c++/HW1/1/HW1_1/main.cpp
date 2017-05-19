205f582856c2a620f4e08bf3246a29b4f89917ed
Jurgen <jazuk.jurij@gmail.com>
2017-05-19 12:25:51 +0300

    program with checking if number is int or not v1.1 (not working)

--- c++/HW1/1/HW1_1/main.cpp
+++ c++/HW1/1/HW1_1/main.cpp
@@ -6,6 +6,7 @@
 */
 
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
@@ -17,15 +18,28 @@ int main()
     int number_index = 0;
 
     cout << "\t Welcome\n";
-    cout << "How many intager numbers do you want to input?\n";
+    cout << "How many integer numbers do you want to input?\n";
     cout << "n = ";
     cin >> numbers_count;
     cout << endl;
 
     for ( int i = 0; i < numbers_count; i++)
     {
-        cout << " Give me intager number [" << i + 1 << "] = ";
+        cout << " Give me integer number [" << i + 1 << "] = ";
         cin >> input_number;
+//        cout << input_number << "\n";
+
+
+        while( cin.fail() ) // if the input number is not digit
+        {
+            cout << input_number << "\n";
+            cout << "Error. Please give me integer number\n";
+            cin.clear();
+            cin.ignore(256,'\n');
+            cout << " Give me integer number [" << i + 1 << "] = ";
+            cin >> input_number;
+
+        }
 
         if ( input_number > max_number )
         {
