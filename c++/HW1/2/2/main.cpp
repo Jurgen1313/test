7c9e1992ad361281ba5a4fe590660183978db274
Jurgen <jazuk.jurij@gmail.com>
2017-05-19 14:21:18 +0300

    second program v1.4 input number without typing number of digits (working version)

--- c++/HW1/2/2/main.cpp
+++ c++/HW1/2/2/main.cpp
@@ -7,51 +7,37 @@ int input_number;
 int digit_count = 0;
 int digit_count_check = 0;
 int sum = 0;
-int temp_var = 0;
+int incorrect_number = 0;
 
 int main()
 {    
     cout << "\t Welcome\n";
-    cout << "How many digit numbers do you want to input?\n";
-    cout << "the number should be uint n = ";
-    cin >> digit_count;
-    cout << endl;
-
-
-    cout << "Type " << digit_count << " digits number : ";
+    cout << " Type uint number : ";
     cin >> input_number;
 
-    temp_var = input_number / 10;
-
-    for (int i = 0; i < pow(2,sizeof(int)*8); i++)
+    if ( input_number >= 0 )
     {
-        if ( temp_var > 0)
-        {
-            digit_count_check++;
-            temp_var /= 10;
-        }
-        else
-        {
-            digit_count_check++;
-            break;
-        }
+        cout << " You type uint number : " << input_number << endl;
 
-    }
-//    cout << "digit in a number = " << digit_count_check << endl;
-
-    if ( digit_count_check == digit_count )
-    {
-        for (int i = 0; i < digit_count ; i++)
+        for (int i = 0; i < pow(2,sizeof(int)*8) ; i++)
         {
-            sum += input_number % 10;
-            input_number = input_number / 10;
+            if ( input_number / 10 > 0)
+            {
+                sum += input_number % 10;
+                input_number = input_number / 10;
+            }
+            else
+            {
+                sum += input_number % 10;
+                break;
+            }
         }
 
-        cout << "Sum = " << sum << endl;
+        cout << "Summ = " << sum << endl;
     }
     else
     {
-        cout << "You gave wrong number" << endl;
+        cout << "NOT uint NUMBER!!!!" << endl;
     }
 
     return 0;
