91ef00738bfb2188f7654ad9067fd6ef21f98710
Jurgen <jazuk.jurij@gmail.com>
2017-05-19 12:54:43 +0300

    second program v1.1 (working version)

--- c++/HW1/2/2/main.cpp
+++ c++/HW1/2/2/main.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int input_number;
+int digit_count;
+int sum = 0;
+int temp_var = 0;
 
 int main()
 {
     cout << "\t Welcome\n";
-    cout << "Type 3 digits number : ";
-    cin >> input_number;
+    cout << "How many digit numbers do you want to input?\n";
+    cout << "n = ";
+    cin >> digit_count;
     cout << endl;
-    cout << input_number;
+
+
+    cout << "Type " << digit_count << " digits number : ";
+    cin >> input_number;
+
+    for (int i = 0; i < digit_count ; i++)
+    {
+        sum += input_number % 10;
+        input_number = input_number / 10;
+    }
+
+    cout << "Sum = " << sum << endl;
+
     return 0;
 }
