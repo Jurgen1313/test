c1d155fe131c4433f3ade5d70c30e76e83a5173f
Jurgen <jazuk.jurij@gmail.com>
2017-06-05 17:52:18 +0300

    ready 1 (new)

--- test_4/test_dynamic_memory/main.cpp
+++ test_4/test_dynamic_memory/main.cpp
@@ -1,9 +1,303 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
 
 using namespace std;
 
-int main(int argc, char *argv[])
+double F_is_this_number (const char symbol)
 {
-    cout << "Hello World!" << endl;
+
+    int i_number;
+    int ui_number;
+    double d_number;
+    bool negative_val = false;
+    size_t size_t_number;
+
+    bool keep_asking = true;
+
+    do
+    {
+        switch (symbol)
+        {
+            case 'i':
+                {
+                    cin >> i_number;
+                    break;
+                }
+            case 'd':
+                {
+                    cin >> d_number;
+                    break;
+                }
+            case 'u':
+                {
+                    cin >> ui_number;
+                    if (ui_number < 0)
+                    {
+                        negative_val = true;
+                        cout << "Negative value. Retype number : ";
+                    }
+                    else
+                        negative_val = false;
+                    break;
+                }
+            case 's':
+                {
+                    cin >> size_t_number;
+                    break;
+                }
+            default:
+                {
+                    cout << "INCORRECT SYMBOL";
+                    break;
+                }
+        }
+        if ( cin.get() != '\n')
+        {
+            cin.clear(); // відновлення прапорця функції cin.fail() в стан good
+            while (cin.get() != '\n');
+            cout << "Invalid value. Retype number : ";
+        }
+        else
+        {
+            if (!negative_val)
+            keep_asking = false;
+        }
+    }while (keep_asking);
+
+    switch (symbol)
+    {
+        case 'i':
+        {
+            return i_number;
+            break;
+        }
+        case 'd':
+        {
+            return d_number;
+            break;
+        }
+        case 'u':
+        {
+            return ui_number;
+            break;
+        }
+        case 's':
+        {
+            return size_t_number;
+            break;
+        }
+        default:
+        {
+            cout << "INCORRECT SYMBOL";
+            return 1;
+            break;
+        }
+    }
+}
+
+bool F_repeat ()
+{
+    bool cycle;
+    char answer;
+    bool input_char = true;
+
+    do
+    {
+        cin >> answer;
+        switch (answer)
+        {
+            case 'Y':
+            case 'y':
+            {
+                input_char = false;
+                cycle = true;
+                break;
+            }
+            case 'N':
+            case 'n':
+            {
+                cycle = false;
+                input_char = false;
+                break;
+            }
+        }
+
+        if (cin.get() != '\n')
+        {
+            while (cin.get() != '\n');
+            input_char = true;
+            cycle = true;
+            cout << "Make correct choise!!!\n";
+        }
+    } while (input_char);
+    return cycle;
+}
+
+void F_print_array (const int size_array, const double *parray)
+{
+    cout << endl;
+    for (int i = 0; i < size_array; ++i)
+        cout << "arr [" << i << "] = " << *(parray + i) << " adress = " <<  parray + i << endl;
+//    for (double* q = parray; q != parray + size_array; ++q)
+//        cout << " " << *q << " " << q << endl;
+}
+
+int main()
+{
+    srand(time(0));
+
+    int task_number = 0;
+    bool keep_asking = true;
+
+    do
+    {
+        system("cls");
+        cout << "\tWellcome to testwork 3 (dynamic memory)" << endl << endl;
+        cout << " Choose task : " << endl;
+        cout << "<1> Create dynamic array and change it size using NEW & DELETE" << endl;
+        cout << "<2> Create dynamic array and change it size using MALLOC, ALLOC, RELLOC" << endl;;
+        cout << "<3> " << endl;
+        cout << "<4> " << endl;
+        cout << "<5> " << endl;
+        cout << "<6> EXIT" << endl << endl;
+        cout << "Make your choise : " ;
+
+        do
+        {
+            task_number = F_is_this_number('u');
+            switch (task_number)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                {
+                    keep_asking = false;
+                    system("cls");
+                    break;
+                }
+                case 6:
+                {
+                    return 0;
+                    break;
+                }
+                default:
+                {
+                    cout << " Retype number : ";
+                    keep_asking = true;
+                    break;
+                }
+            }
+        }while (keep_asking);
+
+        switch (task_number)
+        {
+            case 1:
+            {
+                cout << "How many elements should array have? : ";
+                size_t *array_size = new size_t (F_is_this_number('d'));
+                double *p_array = new (nothrow) double [*array_size] {0};
+
+                if (!p_array)
+                cout << "ERROR creating array" << endl;
+
+                cout << "Do you want manual input? <Y>es or <N>o : ";
+
+                if (F_repeat())
+                {
+                    for(size_t i = 0; i < *array_size; ++i)
+                    {
+                        cout << "Insert number arr[" << i <<"] = ";
+                        p_array[i] = F_is_this_number('d');
+                    }
+                }
+                else
+                    for (size_t i = 0; i < *array_size; ++i)
+                        *(p_array + i) = rand();
+
+                F_print_array(*array_size, p_array);
+
+                cout << endl;
+                cout << "How many ellements should new array have? :";
+
+                size_t *new_array_size = new size_t (F_is_this_number('d'));
+                double *p_copy_array = new double [*new_array_size] {0};
+                for (size_t i = 0; i < *new_array_size; ++i)
+                {
+                    if (i < *array_size )
+                        p_copy_array[i] = p_array[i];
+                    else
+                        p_copy_array[i] = rand();
+                }
+                F_print_array(*new_array_size, p_copy_array);
+                cout << endl;
+                cout << "New created array address = " << p_copy_array << "\nOld array address = " << p_array << endl;
+                delete[] p_array;
+                p_array = 0;
+                p_array = p_copy_array;
+
+                p_copy_array = 0;
+                delete[] p_copy_array;
+//                p_copy_array = 0;
+
+                F_print_array(*new_array_size, p_array);
+
+                if ( p_copy_array != nullptr )
+                    for (size_t i = 0; i < *new_array_size; ++i)
+                        cout << *(p_copy_array + i) << "  " << p_copy_array + i << endl;
+
+                system("pause");
+                // DELETE all dynamic variables
+                delete[] p_array;
+                p_array = 0;
+                delete[] p_copy_array;
+                p_copy_array = 0;
+                delete array_size;
+                array_size = 0;
+                delete new_array_size;
+                new_array_size = 0;
+                break;
+            }
+            case 2:
+            {
+                cout << "How many elements should array have? : ";
+                size_t *array_size = static_cast<size_t*>(malloc(sizeof(int)));
+                *array_size = F_is_this_number('i');
+                cout << "\nArray_size = " << *array_size << endl;
+//                system("pause");
+                double *p_array = static_cast<double*>(calloc(*array_size, sizeof(double)));
+
+                if (!p_array)
+                cout << "ERROR creating array" << endl;
+
+                cout << "Do you want manual input? <Y>es or <N>o : ";
+
+                if (F_repeat())
+                {
+                    for(size_t i = 0; i < *array_size; ++i)
+                    {
+                        cout << "Insert number arr[" << i <<"] = ";
+                        p_array[i] = F_is_this_number('d');
+                    }
+                }
+                else
+                    for (size_t i = 0; i < *array_size; ++i)
+                        *(p_array + i) = rand();
+
+                F_print_array(*array_size, p_array);
+
+                cout << endl;
+                cout << "How many ellements should new array have? :";
+                system("pause");
+//                size_t *new_array_size = new size_t (F_is_this_number('d'));
+//                double *p_copy_array = new double [*new_array_size] {0};
+                break;
+            }
+        }
+    }while(1);
+
     return 0;
 }
